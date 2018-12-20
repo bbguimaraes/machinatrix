67b5c688d079e4036f09dfdf6fd5e53da9bb69d4
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2018-12-20 00:46:06 +0000

    Bard

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <getopt.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -29,6 +30,7 @@ bool cmd_ping(const mtrix_config *config, int argc, const char *const *argv);
 bool cmd_word(const mtrix_config *config, int argc, const char *const *argv);
 bool cmd_abbr(const mtrix_config *config, int argc, const char *const *argv);
 bool cmd_damn(const mtrix_config *config, int argc, const char *const *argv);
+bool cmd_bard(const mtrix_config *config, int argc, const char *const *argv);
 
 mtrix_cmd COMMANDS[] = {
     {"help", cmd_help},
@@ -36,6 +38,7 @@ mtrix_cmd COMMANDS[] = {
     {"word", cmd_word},
     {"abbr", cmd_abbr},
     {"damn", cmd_damn},
+    {"bard", cmd_bard},
     {0, 0},
 };
 
@@ -91,7 +94,8 @@ void usage(FILE *f) {
         "    word:                  random word\n"
         "    damn:                  random curse\n"
         "    abbr <acronym> [<dictionary>]:\n"
-        "                           random de-abbreviation\n",
+        "                           random de-abbreviation\n"
+        "    bard:                  random Shakespearean insult\n",
         PROG_NAME);
 }
 
@@ -277,4 +281,137 @@ bool cmd_damn(const mtrix_config *config, int argc, const char *const *argv) {
     if(buffer)
         free(buffer);
     return wait_n(1);
+}
+
+bool cmd_bard(const mtrix_config *config, int argc, const char *const *argv) {
+    if(argc) {
+        log_err("wrong number of arguments (%i, want 0)\n", argc);
+        return false;
+    }
+    static const char *v[] = {
+        "A most notable coward, an infinite and endless liar, an hourly"
+            " promise breaker, the owner of no one good quality.\n"
+            "-- All’s Well That Ends Well (Act 3, Scene 6)\n",
+        "Away, you starvelling, you elf-skin, you dried neat’s-tongue,"
+            " bull’s-pizzle, you stock-fish!\n"
+            "-- Henry IV Part I (Act 2, Scene 4)\n",
+        "Away, you three-inch fool!\n"
+            "-- The Taming of the Shrew (Act 3, Scene 3)\n",
+        "Come, come, you froward and unable worms!\n"
+            "-- The Taming Of The Shrew (Act 5, Scene 2)\n",
+        "Go, prick thy face, and over-red thy fear, Thou lily-liver’d boy.\n"
+            "-- Macbeth (Act 5, Scene 3)\n",
+        "His wit’s as thick as a Tewkesbury mustard.\n"
+            "-- Henry IV Part 2 (Act 2, Scene 4)\n",
+        "I am pigeon-liver’d and lack gall.\n"
+            "-- Hamlet (Act 2, Scene 2)\n",
+        "I am sick when I do look on thee\n"
+            "-- A Midsummer Night’s Dream (Act 2, Scene 1)\n",
+        "I must tell you friendly in your ear, sell when you can, you are not"
+            " for all markets.\n"
+            "-- As You Like It (Act 3 Scene 5)\n",
+        "If thou wilt needs marry, marry a fool; for wise men know well enough"
+            " what monsters you make of them.\n"
+            "-- Hamlet (Act 3, Scene 1)\n",
+        "I’ll beat thee, but I would infect my hands.\n"
+            "-- Timon of Athens (Act 4, Scene 3)\n",
+        "I scorn you, scurvy companion.\n"
+            "-- Henry IV Part II (Act 2, Scene 4)\n",
+        "Methink’st thou art a general offence and every man should beat thee."
+            "\n"
+            "-- All’s Well That Ends Well (Act 2, Scene 3)\n",
+        "More of your conversation would infect my brain.\n"
+            "-- The Comedy of Erros (Act 2, Scene 1)\n",
+        "My wife’s a hobby horse!\n"
+            "-- The Winter’s Tale (Act 2, Scene 1)\n",
+        "Peace, ye fat guts!\n"
+            "-- Henry IV Part 1 (Act 2, Scene 2)\n",
+        "Poisonous bunch-backed toad! \n"
+            "-- Richard III (Act 1, Scene 3)\n",
+        "The rankest compound of villainous smell that ever offended nostril\n"
+            "-- The Merry Wives of Windsor (Act 3, Scene 5)\n",
+        "The tartness of his face sours ripe grapes.\n"
+            "-- The Comedy of Erros (Act 5, Scene 4)\n",
+        "There’s no more faith in thee than in a stewed prune.\n"
+            "-- Henry IV Part 1 (Act 3, Scene 3)\n",
+        "Thine forward voice, now, is to speak well of thine friend; thine"
+            " backward voice is to utter foul speeches and to detract.\n"
+            "-- The Tempest (Act 2, Scene 2)\n",
+        "That trunk of humours, that bolting-hutch of beastliness, that"
+            " swollen parcel of dropsies, that huge bombard of sack, that"
+            " stuffed cloak-bag of guts, that roasted Manningtree ox with"
+            " pudding in his belly, that reverend vice, that grey Iniquity,"
+            " that father ruffian, that vanity in years?\n"
+            "-- Henry IV Part 1 (Act 2, Scene 4)\n",
+        "Thine face is not worth sunburning.\n"
+            "-- Henry V (Act 5, Scene 2)\n",
+        "This woman’s an easy glove, my lord, she goes off and on at"
+            " pleasure.\n"
+            "-- All’s Well That Ends Well (Act 5, Scene 3)\n",
+        "Thou art a boil, a plague sore\n"
+            "-- King Lear (Act 2, Scene 2)\n",
+        "Was the Duke a flesh-monger, a fool and a coward?\n"
+            "-- Measure For Measure (Act 5, Scene 1)\n",
+        "Thou art as fat as butter.\n"
+            "-- Henry IV Part 1 (Act 2, Scene 4)\n",
+        "Here is the babe, as loathsome as a toad.\n"
+            "-- Titus Andronicus (Act 4, Scene 3)\n",
+        "Like the toad; ugly and venomous.\n"
+            "-- As You Like It (Act 2, Scene 1`)\n",
+        "Thou art unfit for any place but hell.\n"
+            "-- Richard III (Act 1 Scene 2)\n",
+        "Thou cream faced loon\n"
+            "-- Macbeth (Act 5, Scene 3)\n",
+        "Thou clay-brained guts, thou knotty-pated fool, thou whoreson obscene"
+            " greasy tallow-catch!\n"
+            "-- Henry IV Part 1 (Act 2, Scene 4 )\n",
+        "Thou damned and luxurious mountain goat.\n"
+            "-- Henry V (Act 4, Scene 4)\n",
+        "Thou elvish-mark’d, abortive, rooting hog!\n"
+            "-- Richard III (Act 1, Scene 3 )\n",
+        "Thou leathern-jerkin, crystal-button, knot-pated, agatering,"
+            " puke-stocking, caddis-garter, smooth-tongue, Spanish pouch!\n"
+            "-- Henry IV Part 1 (Act 2, Scene 4)\n",
+        "Thou lump of foul deformity\n"
+            "-- Richard III (Act 1, Scene 2)\n",
+        "That poisonous bunch-back’d toad!\n"
+            "-- Richard III (Act 1, Scene 3)\n",
+        "Thou sodden-witted lord! Thou hast no more brain than I have in mine"
+            " elbows\n"
+            "-- Troilus and Cressida (Act 2, Scene 1)\n",
+        "Thou subtle, perjur’d, false, disloyal man!\n"
+            "-- The Two Gentlemen of Verona (Act 4, Scene 2)\n",
+        "Thou whoreson zed , thou unnecessary letter!\n"
+            "-- King Lear (Act 2, Scene 2)\n",
+        "Thy sin’s not accidental, but a trade.\n"
+            "-- Measure For Measure (Act 3, Scene 1)\n",
+        "Thy tongue outvenoms all the worms of Nile.\n"
+            "-- Cymbeline (Act 3, Scene 4)\n",
+        "Would thou wert clean enough to spit upon\n"
+            "-- Timon of Athens (Act 4, Scene 3)\n",
+        "Would thou wouldst burst!\n"
+            "-- Timon of Athens (Act 4, Scene 3)\n",
+        "You poor, base, rascally, cheating lack-linen mate! \n"
+            "-- Henry IV Part II (Act 2, Scene 4)\n",
+        "You are as a candle, the better burnt out.\n"
+            "-- Henry IV Part 2 (Act 1, Scene 2)\n",
+        "You scullion! You rampallian! You fustilarian! I’ll tickle your"
+            " catastrophe!\n"
+            "-- Henry IV Part 2 (Act 2, Scene 1)\n",
+        "You starvelling, you eel-skin, you dried neat’s-tongue, you"
+            " bull’s-pizzle, you stock-fish–O for breath to utter what is like"
+            " thee!-you tailor’s-yard, you sheath, you bow-case, you vile"
+            " standing tuck!\n"
+            "-- Henry IV Part 1 (Act 2, Scene 4)\n",
+        "Your brain is as dry as the remainder biscuit after voyage.\n"
+            "-- – As You Like It (Act 2, Scene 7)\n",
+        "Virginity breeds mites, much like a cheese.\n"
+            "-- All’s Well That Ends Well (Act 1, Scene 1)\n",
+        "Villain, I have done thy mother\n"
+            "-- Titus Andronicus (Act 4, Scene 2)\n",
+    };
+    static const size_t n = sizeof(v) / sizeof(*v);
+    srand(time(NULL));
+    printf("%s", v[rand() % n]);
+    return true;
 }
